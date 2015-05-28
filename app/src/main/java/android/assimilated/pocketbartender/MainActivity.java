68323fb2baf70a6b177deb4da9f12790def30eb6
viv <vivyanw@uw.edu>
2015-05-28 13:01:57 -0700

    enabled main activity to access application

--- app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
+++ app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
@@ -8,10 +8,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    MainApp app;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app = (MainApp) getApplication();
     }
 
 
