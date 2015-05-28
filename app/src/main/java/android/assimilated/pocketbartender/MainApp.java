6b2e2bd317d033b38a3b33d38c0bf5ec753b5dbf
viv <vivyanw@uw.edu>
2015-05-28 13:01:23 -0700

    added way to get current date

--- app/src/main/java/android/assimilated/pocketbartender/MainApp.java
+++ app/src/main/java/android/assimilated/pocketbartender/MainApp.java
@@ -2,22 +2,28 @@ package android.assimilated.pocketbartender;
 
 import android.app.Application;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by iguest on 5/28/15.
  */
 public class MainApp extends Application {
+    public static int today;
 
     public MainApp() {
         //ensures that there is only one instance of QuizApp
         if (instance == null) {
             instance = this;
         } else {
-            throw new RuntimeException("Cannot create more than one QuizApp");
+            throw new RuntimeException("Cannot create more than one MainApp");
         }
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
     }
 }
