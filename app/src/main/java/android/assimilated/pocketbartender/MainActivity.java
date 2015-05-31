c723201175ca9b01510776d3610c0c572b5c78a2
Eden Ghirmai <ghirme@uw.edu>
2015-05-31 16:29:57 -0700

    Update MainActivity.java

--- app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
+++ app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
@@ -24,7 +24,62 @@ public class MainActivity extends ActionBarActivity {
             //update drink of the day
             app.today = day;
         }
+        
+        // Gets all the JSON goodiez
+        // Has to be done in its own thread or shit hits the fan
+        Thread thread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    // grabbing JSON files from student server
+                    String ingredientsJSON = getJSON("http://students.washington.edu/ghirme/info498c/ingredients.json");
+                    String recipesJSON = getJSON("http://students.washington.edu/ghirme/info498c/recipes.json");
+
+                    try{
+                        JSONObject ingredientsObject = new JSONObject(ingredientsJSON);
+                        JSONObject recipesObject = new JSONObject(recipesJSON);
+                    } catch(Exception e){
+                        e.printStackTrace();
+                    } finally {
+                        // Do stuff with ingredients/recipes JSON objects
+                    };
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();        
+        
     }
+    
+    // takes a URL String and returns the JSON as a string
+    public String getJSON(String address){
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(address);
+        try{
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if(statusCode == 200){
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while((line = reader.readLine()) != null){
+                    builder.append(line);
+                }
+            } else {
+                Log.e(MainActivity.class.toString(), "Failedet JSON object");
+            }
+        }catch(ClientProtocolException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return builder.toString();
+    }    
 
 
     @Override
