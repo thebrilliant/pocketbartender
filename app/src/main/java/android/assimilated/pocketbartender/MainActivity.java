40500a6808bbe36a64dd50ed21bf418da5a5e647
thebrilliant <vivyanw@uw.edu>
2015-06-02 10:09:16 -0700

    added code to go to result activity once user submits search query

--- app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
+++ app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
@@ -1,10 +1,26 @@
 package android.assimilated.pocketbartender;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Calendar;
 
 
@@ -12,6 +28,9 @@ public class MainActivity extends ActionBarActivity {
 
     MainApp app;
     int day;
+
+    SearchView search;
+    String searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +43,9 @@ public class MainActivity extends ActionBarActivity {
             //update drink of the day
             app.today = day;
         }
-        
+
+        search = (SearchView) findViewById(R.id.searchView);
+
         // Gets all the JSON goodiez
         // Has to be done in its own thread or shit hits the fan
         Thread thread = new Thread(new Runnable(){
@@ -49,7 +70,27 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        thread.start();        
+        thread.start();
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                searchText = s;
+                Intent next = new Intent(MainActivity.this, ResultActivity.class);
+                next.putExtra("search", searchText);
+
+                Log.i("MainActivity", "Firing search intent: " + next);
+                startActivity(next);
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchText = s;
+                return false;
+            }
+        });
         
     }
     
