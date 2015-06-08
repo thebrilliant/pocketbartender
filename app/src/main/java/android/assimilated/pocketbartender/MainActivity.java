5ee4c758395999386a21251fce230a530bacab1a
thebrilliant <vivyanw@uw.edu>
2015-06-08 12:26:18 -0700

    added to result activity for accessing results

--- app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
+++ app/src/main/java/android/assimilated/pocketbartender/MainActivity.java
@@ -1,21 +1,11 @@
 package android.assimilated.pocketbartender;
 
 import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.SearchView;
+import android.view.*;
+import android.widget.*;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-//import android.support.v7.widget.SearchView;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -27,10 +17,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Calendar;
 
 
--- app/src/main/java/android/assimilated/pocketbartender/ResultActivity.java
+++ app/src/main/java/android/assimilated/pocketbartender/ResultActivity.java
@@ -1,19 +1,50 @@
 package android.assimilated.pocketbartender;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by iguest on 5/28/15.
  */
 public class ResultActivity extends ActionBarActivity {
 
+    MainApp app;
+
+    TextView userText;
+
+    String userSearch;
+    String type;
+    Ligst<Recipe> results;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.results_layout);
+
+        app = (MainApp) getApplication();
+
+        Intent launchingIntent = getIntent();
+        userSearch = launchingIntent.getStringExtra("search");
+        type = launchingIntent.getStringExtra("type");
+
+        userText = (TextView) findViewById(R.id.txtSearch);
+
+        userText.setText(userSearch);
+
+        if (type.equalsIgnoreCase("name")) {
+            app.searchByName(userSearch);
+        } else if (type.equalsIgnoreCase("ingredients")) {
+            app.searchByIngredient(userSearch);
+        } else {
+            double cost = Double.parseDouble(userSearch);
+            app.searchByAmount(cost);
+        }
     }
 
 
